c46f33a86e93a758a3a2172d5bb9d18e985bd1b1
Cezary Hodun <cezaryh1106@gmail.com>
2021-12-10 19:00:27 +0100

    exception

---
+++ Lollipop.Application/Exceptions/EntityNotFoundException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lollipop.Application.Exceptions
+{
+    public class EntityNotFoundException : Exception
+    {
+        public EntityNotFoundException(Type type, string id)
+            : base($"Entity of type {type} and id {id} was not found.")
+        {
+        }
+    }
+}
