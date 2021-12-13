54f42a03ec5ef97617e15131864efa6fa95e9b33
Przemysław Klejno <pemix09@onet.pl>
2021-12-13 23:37:58 +0100

    Forbidden word CRUD

--- Lollipop.API/Controllers/ForbiddenWordController.cs
+++ Lollipop.API/Controllers/ForbiddenWordController.cs
@@ -6,6 +6,7 @@
     using System.Threading.Tasks;
     using Lollipop.Application.ForbiddenWord.Queries;
     using Lollipop.Core.Models;
+    using Lollipop.Application.ForbiddenWord.Commands;
 
     [ApiController]
     [Route("[controller]/[action]")]
@@ -20,6 +21,22 @@
         [HttpGet]
         public async Task<ActionResult<IEnumerable<ForbiddenWord>>> GetForbiddenWords([FromQuery] GetForbiddenWordsQuery query) =>
             Ok(await _mediator.Send(query));
+
+        [HttpGet]
+        public async Task<ActionResult<ForbiddenWord>> GetForbiddenWordById([FromQuery] GetForbiddenWordsByIdQuery query) =>
+            Ok(await _mediator.Send(query));
+
+        [HttpDelete]
+        public async Task<int> DeleteForbiddenWordById([FromBody] DeleteForbiddenWordByIdCommand command) =>
+            await _mediator.Send(command);
+
+        [HttpPut]
+        public async Task<int> UpdateForbiddenWord([FromBody] UpdateForbiddenWordCommand command) =>
+            await _mediator.Send(command);
+
+        [HttpPost]
+        public async Task<int> CreateForbiddenWord([FromBody] CreateForbiddenWord command) =>
+            await _mediator.Send(command);
 
     }
 }
---
+++ Lollipop.Application/ForbiddenWord/Commands/CreateForbiddenWord.cs
@@ -0,0 +1,31 @@
+﻿namespace Lollipop.Application.ForbiddenWord.Commands
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Lollipop.Application.Repository;
+    using Lollipop.Core.Models;
+    using MediatR;
+
+    public class CreateForbiddenWord : IRequest<int>
+    {
+        public string word { get; set; }
+        public class Handler : IRequestHandler<CreateForbiddenWord, int>
+        {
+            private readonly IRepository<ForbiddenWord> _repository;
+            public Handler(IRepository<ForbiddenWord> repository)
+            {
+                _repository = repository;
+            }
+            public async Task<int> Handle(CreateForbiddenWord request, CancellationToken cancellationToken)
+            {
+                ForbiddenWord word = ForbiddenWord.Create(request.word);
+                await _repository.AddAsync(word);
+                return word.Id;
+            }
+        }
+    }
+}
