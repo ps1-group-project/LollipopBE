38a638ee87d4bd77dabc155ff1ccfa6d685b81e5
Cezary Hodun <cezaryh1106@gmail.com>
2021-12-18 18:04:15 +0100

    added userservice

---
+++ Lollipop.API/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MediatR;
+using Lollipop.Application.Keyword.Commands;
+using Lollipop.Application.Keyword.Queries;
+using System.Collections.Generic;
+using Lollipop.Core.Models;
+
+namespace Lollipop.API.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class UserController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public UserController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+    }
+}
