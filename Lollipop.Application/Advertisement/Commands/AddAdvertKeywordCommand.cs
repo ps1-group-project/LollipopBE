ccc83e16ac77c41870f049d7051c978459f53f86
Przemysław Klejno <pemix09@onet.pl>
2022-01-22 14:00:49 +0100

    Add and Delete for Advertisement's keywords

---
+++ Lollipop.Application/Advertisement/Commands/AddAdvertKeywordCommand.cs
@@ -0,0 +1,42 @@
+﻿namespace Lollipop.Application.Advertisement.Commands
+{
+    using Lollipop.Application.Repository;
+    using Lollipop.Core.Models;
+    using MediatR;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Lollipop.Application.Advertisement.Validators;
+    using FluentValidation;
+
+    public class AddAdvertKeywordCommand : IRequest<int>
+    {
+        public int AdvertId { get; set; }
+        public int KeywordId { get; set; }
+        public class Handler : IRequestHandler<AddAdvertKeywordCommand, int>
+        {
+            private readonly IRepository<Advertisement> _advertisementRepository;
+            private readonly IRepository<Keyword> _keywordRepository;
+            public Handler(IRepository<Advertisement> advertisementRepository, IRepository<Keyword> keywordRepository)
+            {
+                _advertisementRepository = advertisementRepository;
+                _keywordRepository = keywordRepository;
+            }
+            public async Task<int> Handle(AddAdvertKeywordCommand request, CancellationToken cancellationToken)
+            {
+                await new AddKeywordValidator().ValidateAndThrowAsync(request, cancellationToken);
+
+                Advertisement toUpdate = await  _advertisementRepository.GetByIdAsync(request.AdvertId);
+                Keyword keyword = await _keywordRepository.GetByIdAsync(request.KeywordId);
+
+                toUpdate.AddKeyword(keyword);
+                await _advertisementRepository.UpdateAsync(toUpdate);
+
+                return keyword.Id;
+            }
+        }
+    }
+}
---
+++ Lollipop.Application/Advertisement/Commands/DeleteAdvertKeywordCommand.cs
@@ -0,0 +1,42 @@
+﻿namespace Lollipop.Application.Advertisement.Commands
+{
+    using Lollipop.Application.Repository;
+    using MediatR;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Lollipop.Core.Models;
+    using System.Threading;
+    using Lollipop.Application.Advertisement.Validators;
+    using FluentValidation;
+
+    public class DeleteAdvertKeywordCommand : IRequest<int>
+    {
+        public int AdvertId { get; set; }
+        public int KeywordId { get; set; }
+        public class Handler : IRequestHandler<DeleteAdvertKeywordCommand, int>
+        {
+            private readonly IRepository<Advertisement> _advertisementRepository;
+            private readonly IRepository<Keyword> _keywordRepository;
+            public Handler(IRepository<Advertisement> advertisementRepository, IRepository<Keyword> keywordRepository)
+            {
+                _advertisementRepository = advertisementRepository;
+                _keywordRepository = keywordRepository;
+            }
+            public async Task<int> Handle(DeleteAdvertKeywordCommand request, CancellationToken cancellationToken)
+            {
+                await new DeleteKeywordValidator().ValidateAndThrowAsync(request, cancellationToken);
+
+                Advertisement toUpdate = await _advertisementRepository.GetByIdAsync(request.AdvertId);
+                Keyword keyword = await _keywordRepository.GetByIdAsync(request.KeywordId);
+
+                toUpdate.RemoveKeyword(keyword);
+                await _advertisementRepository.UpdateAsync(toUpdate);
+
+                return keyword.Id;
+            }
+        }
+    }
+}
